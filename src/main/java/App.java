17d88f939d4682b56a29a187f721a1823ac4af05
Evan Bishop and Janek Brandt <janek@>
2017-03-16 15:30:38 -0700

    add check to prevent empty guesses

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,6 +10,45 @@ public class App {
     boolean gameNotOver = true;
     Integer guessLimit = 5;
 
+    while (gameNotOver) {
+      printMonster(newGame.wrongGuesses(), guessLimit);
+      System.out.println("\n");
+      System.out.println(newGame.getHiddenWord());
+      System.out.println("Letters guessed: " + newGame.getLettersGuessed());
+      System.out.println("Wrong Guesses: " + newGame.wrongGuesses() + "/" + guessLimit);
+      String userGuess = console.readLine("Guess next letter: ");
+      System.out.println("-----------------------------");
+      if (!userGuess.equals("")) {
+        newGame.guessLetter(userGuess.charAt(0));
+      }
+
+      if (newGame.getHiddenWord().indexOf('_') == -1) {
+        System.out.println("\nYou Won!\tThe word was:\t" + newGame.getWord() + "!\n");
+        String playAgain = console.readLine("Play again?\n");
+        if (playAgain.equals("yes")) {
+          console.flush();
+          newGame = new Game(randomGenerator.nextInt(wordCount));
+        } else if (playAgain.equals("no")) {
+          gameNotOver = false;
+        }
+      }
+
+      if (newGame.wrongGuesses() == guessLimit) {
+        printMonster(newGame.wrongGuesses(), guessLimit);
+        System.out.println("You Lost!\tThe word was:\t" + newGame.getWord() + "!");
+        String playAgain = console.readLine("Play again?\t");
+        if (playAgain.equals("yes")) {
+          console.flush();
+          newGame = new Game(randomGenerator.nextInt(wordCount));
+        } else if (playAgain.equals("no")) {
+          gameNotOver = false;
+        }
+      }
+
+    }
+  }
+
+  public static void printMonster(int guesses, int limit) {
     String[] monster = {"######################~~..'|.##############.|`..~~#######################",
     "##############~./`.~~./' ./ ################ \\. `\\. ~~.`\\.~##############",
     "############~.' `.`-'   /   ~#############~ .  \\   `-'.'  `.~############",
@@ -33,42 +72,9 @@ public class App {
     "##.`       '#####\\__~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~__/#####`      '.## ",
     "###        #######  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  #######       ### "};
 
-    while (gameNotOver) {
-      System.out.println("\n");
-      System.out.println(newGame.getHiddenWord());
-      System.out.println("Letters guessed: " + newGame.getLettersGuessed());
-      System.out.println("Wrong Guesses: " + newGame.wrongGuesses() + "/" + guessLimit);
-      char userGuess = console.readLine("Guess next letter: ").charAt(0);
-      System.out.println("-----------------------------");
-      newGame.guessLetter(userGuess);
-
-
-      for (int i = 0; i < newGame.wrongGuesses() * monster.length/guessLimit; i++) {
-        System.out.println(monster[i]);
-      }
-
-      if (newGame.getHiddenWord().indexOf('_') == -1) {
-        System.out.println("\nYou Won!");
-        System.out.println("The word was:\t" + newGame.getWord() + "!\n");
-        String playAgain = console.readLine("Play again?\n");
-        if (playAgain.equals("yes")) {
-          newGame = new Game(randomGenerator.nextInt(wordCount));
-        } else if (playAgain.equals("no")) {
-          gameNotOver = false;
-        }
-      }
-
-      if (newGame.wrongGuesses() == guessLimit) {
-        System.out.println("\nYou Lost!\n");
-        System.out.println("The word was:\t" + newGame.getWord() + "!\n");
-        String playAgain = console.readLine("Play again?\n");
-        if (playAgain.equals("yes")) {
-          newGame = new Game(randomGenerator.nextInt(wordCount));
-        } else if (playAgain.equals("no")) {
-          gameNotOver = false;
-        }
-      }
-
+    System.out.print("\033[H\033[2J");
+    for (int i = 0; i < guesses * monster.length/limit; i++) {
+      System.out.println(monster[i]);
     }
   }
 }
